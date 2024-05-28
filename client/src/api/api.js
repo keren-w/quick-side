990bd335fc0db3ff33c0df7babc5428cd15470c6
Keren Wigelman <keren.w@gmail.com>
2024-05-28 03:01:07 -0400

    init project

---
+++ client/src/api/api.js
@@ -0,0 +1,9 @@
+// import resourcesList from '@/api/newData.json';
+
+// export const getResourcesList = async () => new Promise((resolve) => (setTimeout(() => resolve(resourcesList), 1000)));
+
+// export const getResourcesListApi = async () => {
+
+//     const res = await fetch('/api/resources');
+//     return res.json();
+// }
---
+++ server/routes/api.js
@@ -0,0 +1,10 @@
+// var express = require('express');
+// var router = express.Router();
+// var resouceList = require('../data/newData.json');
+
+// /* GET users listing. */
+// router.get('/resources', function(req, res, next) {
+//   res.json(resouceList);
+// });``
+
+// module.exports = router;
